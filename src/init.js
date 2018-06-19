09999a82b849bcb04400825f91142e0a11fbfb83
lhuynh27 <leannehuynh27@gmail.com>
2018-06-19 12:46:51 -0700

    Add event handlers for lineup and mouseover

--- src/init.js
+++ src/init.js
@@ -28,6 +28,19 @@ $(document).ready(function() {
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+    window.dancers.push(dancer);
+    console.log(window.dancers);
   });
+  
+  $('.lineUpButton').on('click', function(event) {
+    for(var i = 0; i < window.dancers.length; i++){
+      window.dancers[i].lineUp(i);
+    }
+    
+  });
+  $('.fading').on('mouseover', function(event) {
+    $('.fading').hide();
+  });
+
 });
 
