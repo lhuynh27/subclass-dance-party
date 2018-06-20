7e7da76324d57120b6ffcd2c32a2fd259f67102f
lhuynh27 <leannehuynh27@gmail.com>
2018-06-19 19:01:58 -0700

    Move lineUp method to superclass

--- src/dancer.js
+++ src/dancer.js
@@ -25,5 +25,8 @@ makeDancer.prototype.setPosition = function(top, left) {
   this.$node.css(styleSettings);
 };
 
+makeDancer.prototype.lineUp = function(offset) {
+  this.setPosition(300, 100 + (offset * 150));
+};
 // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
 // this one sets the position to some random  default point within the body
